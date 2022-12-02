e9cb99a9128f4030f2aeb25b90584753b41a7e7f
Victor Ughonu <victorughonu@gmail.com>
2022-12-02 16:51:13 +0100

    finished with the footer section

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,62 @@
-'use client';
+"use client";
+
+import { motion } from "framer-motion";
+import styles from "../styles";
+import { footerVariants } from "../utils/motion";
+import { TitleText } from "./CustomTexts";
+import { socials } from "../constants";
 
 const Footer = () => (
-  <footer>
-    footer
-  </footer>
+  <motion.footer
+    variants={footerVariants}
+    initial="hidden"
+    whileInView="show"
+    className={`${styles.paddings} py-8 relative z-10`}
+  >
+    <div className="footer-gradient" />
+    <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
+      <div className="flex flex-wrap items-center justify-between gap-5">
+        <h4 className="font-bold text-white md:text-[64px] text-[44px] leading-[81px]">
+          Enter the Metaverse
+        </h4>
+        <button
+          type="button"
+          className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] flex-nowrap gap-[12px]"
+        >
+          <img
+            src="/headset.svg"
+            alt="headset"
+            className="w-[24px] h-[24px] object-contain"
+          />
+          <span className="text-[16px] text-white font-normal">
+            ENTER METAVERSE
+          </span>
+        </button>
+      </div>
+
+      <div className="flex flex-col">
+        <div className="mb-[50px] h-[2px] bg-white opacity-10" />
+        <div className="flex flex-wrap items-center justify-between gap-4">
+          <h4 className="text-[24px] leading-[30px] font-extrabold uppercase text-white">
+            METAVERUS
+          </h4>
+          <p className="text-[14px] font-normal text-white opacity-50">
+            Copyright © 2021 - 2022 Metaversus. All rights reserved.
+          </p>
+          <div className="flex gap-4">
+            {socials.map((social, index) => (
+              <img
+                key={social.name}
+                src={social.url}
+                alt={social.name}
+                className="text-[#F1F5F8] w-[24px] h-[24px] object-contain cursor-pointer"
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  </motion.footer>
 );
 
 export default Footer;
